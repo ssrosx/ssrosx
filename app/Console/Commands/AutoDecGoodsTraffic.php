4db176f77f82c015ee56ef8ab1183c1b8d94c4c2
Main <main@MaintekiMacBook-Pro.local>
2018-09-13 09:58:19 -0700

    backup learning
    backup learning

--- app/Console/Commands/AutoDecGoodsTraffic.php
+++ app/Console/Commands/AutoDecGoodsTraffic.php
@@ -37,70 +37,69 @@ class AutoDecGoodsTraffic extends Command
 
             DB::beginTransaction();
             try {
-            foreach ($orderList as $order) {
-                Order::query()->where('oid', $order->oid)->update(['is_expire' => 1]);
+                foreach ($orderList as $order) {
+                    Order::query()->where('oid', $order->oid)->update(['is_expire' => 1]);
 
-                if (empty($order->user) || empty($order->goods)) {
-                    continue;
-                }
-
-                // 到期自动处理
-                if ($order->expire_at/* && date("Y-m-d H:i:s") >= $order->expire_at*/) {
-                    $traffic = $order->traffic * 1048576;
-                    if ($traffic == 0)
-                    {
-                        $traffic = $order->goods->traffic * 1048576;
-                    }
-
-                    if ($traffic == 0)
-                    {
+                    if (empty($order->user) || empty($order->goods)) {
                         continue;
                     }
 
-                    if ($order->user->transfer_enable - $traffic <= 0) {
-                        User::query()->where('id', $order->user_id)->update(['transfer_enable' => 0, 'u' => 0, 'd' => 0]);
-                    }
-                    else if ($order->user->d - $traffic >= 0)
-                    {
-                        User::query()->where('id', $order->user_id)->decrement('transfer_enable', $traffic);
-                        User::query()->where('id', $order->user_id)->decrement('d', $traffic);
-                    }
-                    else
-                    {
-                        if ($order->user->u + $order->user->d - $traffic <= 0)
+                    // 到期自动处理
+                    if ($order->expire_at) {
+                        $traffic = $order->traffic * 1048576;
+                        if ($traffic == 0)
+                        {
+                            $traffic = $order->goods->traffic * 1048576;
+                        }
+
+                        if ($traffic == 0)
+                        {
+                            continue;
+                        }
+
+                        if ($order->user->transfer_enable - $traffic <= 0) {
+                            User::query()->where('id', $order->user_id)->update(['transfer_enable' => 0, 'u' => 0, 'd' => 0]);
+                        }
+                        else if ($order->user->d - $traffic >= 0)
                         {
                             User::query()->where('id', $order->user_id)->decrement('transfer_enable', $traffic);
-                            User::query()->where('id', $order->user_id)->update(['u' => 0, 'd' => 0]);
+                            User::query()->where('id', $order->user_id)->decrement('d', $traffic);
                         }
                         else
                         {
-                            User::query()->where('id', $order->user_id)->decrement('transfer_enable', $traffic);
-                            User::query()->where('id', $order->user_id)->decrement('u', $traffic - $order->user->d);
-                            User::query()->where('id', $order->user_id)->update(['d' => 0]);
+                            if ($order->user->u + $order->user->d - $traffic <= 0)
+                            {
+                                User::query()->where('id', $order->user_id)->decrement('transfer_enable', $traffic);
+                                User::query()->where('id', $order->user_id)->update(['u' => 0, 'd' => 0]);
+                            }
+                            else
+                            {
+                                User::query()->where('id', $order->user_id)->decrement('transfer_enable', $traffic);
+                                User::query()->where('id', $order->user_id)->decrement('u', $traffic - $order->user->d);
+                                User::query()->where('id', $order->user_id)->update(['d' => 0]);
+                            }
+                        }
+
+                        // 删除该商品对应用户的所有标签
+                        UserLabel::query()->where('user_id', $order->user->id)->delete();
+
+                        // 取出用户的其他商品带有的标签
+                        $goodsIds = Order::query()->where('user_id', $order->user->id)->where('oid', '<>', $order->oid)->where('status', 2)->where('is_expire', 0)->groupBy('goods_id')->pluck('goods_id')->toArray();
+                        $goodsLabels = GoodsLabel::query()->whereIn('goods_id', $goodsIds)->groupBy('label_id')->pluck('label_id')->toArray();
+
+                        // 生成标签
+                        $labels = array_values(array_unique(array_merge($goodsLabels, $defaultLabels))); // 标签去重
+                        foreach ($labels as $vo) {
+                            $userLabel = new UserLabel();
+                            $userLabel->user_id = $order->user->id;
+                            $userLabel->label_id = $vo;
+                            $userLabel->save();
                         }
                     }
-
-                    // 删除该商品对应用户的所有标签
-                    UserLabel::query()->where('user_id', $order->user->id)->delete();
-
-                    // 取出用户的其他商品带有的标签
-                    $goodsIds = Order::query()->where('user_id', $order->user->id)->where('oid', '<>', $order->oid)->where('status', 2)->where('is_expire', 0)->groupBy('goods_id')->pluck('goods_id')->toArray();
-                    $goodsLabels = GoodsLabel::query()->whereIn('goods_id', $goodsIds)->groupBy('label_id')->pluck('label_id')->toArray();
-
-                    // 生成标签
-                    $labels = array_values(array_unique(array_merge($goodsLabels, $defaultLabels))); // 标签去重
-                    foreach ($labels as $vo) {
-                        $userLabel = new UserLabel();
-                        $userLabel->user_id = $order->user->id;
-                        $userLabel->label_id = $vo;
-                        $userLabel->save();
-                    }
                 }
-                
                 DB::commit();
             } catch (\Exception $e) {
                 \Log::error($this->description . '：' . $e);
-
                 DB::rollBack();
             }
         }
